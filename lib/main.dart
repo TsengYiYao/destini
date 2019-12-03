dc908edd1e38cee62c9c076d1b8eccca002c1a4c
TsengYiYao <yiyatsen@gmail.com>
2019-12-03 15:40:52 +0800

    no message

--- lib/main.dart
+++ lib/main.dart
@@ -75,19 +75,22 @@ class _StoryPageState extends State<StoryPage> {
                 flex: 2,
                 //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
                 //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
-                child: FlatButton(
-                  onPressed: () {
-                    //Choice 2 made by user.
-                    //TODO:x Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
-                    print('storyBrain.nextStory(2);');
-                    storyBrain.nextStory(2);
-                  },
-                  color: Colors.blue,
-                  child: Text(
-                    //TODO: Step 14x - Use the storyBrain to get the text for choice 2.
-                    storyBrain.getChoice2(),
-                    style: TextStyle(
-                      fontSize: 20.0,
+                child: Visibility(
+                  visible: storyBrain.buttonShouldBeVisible(),
+                  child: FlatButton(
+                    onPressed: () {
+                      //Choice 2 made by user.
+                      //TODO:x Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
+                      print('storyBrain.nextStory(2);');
+                      storyBrain.nextStory(2);
+                    },
+                    color: Colors.blue,
+                    child: Text(
+                      //TODO: Step 14x - Use the storyBrain to get the text for choice 2.
+                      storyBrain.getChoice2(),
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
                     ),
                   ),
                 ),
